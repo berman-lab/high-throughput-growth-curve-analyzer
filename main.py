97278c7e9693217fa908e63159717fa76cd62308
Carmi-Y <yeffet.carmi@gmail.com>
2021-10-27 12:45:43 +0300

    Usage of dataclass

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from experiment_data import ExperimentData 
 
 def main():
 
@@ -41,8 +42,8 @@ def read_data(input_directory, extensions):
                 # Get the name of the current file. The last part of the path then remove the file extension
                 curr_file_name = excel_file_location.split('/')[-1].split(".")[0]
                 # Create a new object to save data into
-                # TODO change to dataclass?
-                parsed_data.append({ 'ODs': {}, 'times': [], 'temps': [], 'plate_name' : sheet, 'file_name': curr_file_name })
+                
+                parsed_data.append(ExperimentData({}, [], [], sheet, curr_file_name))
 
                 # Load the current sheet of the excel file
                 df = pd.read_excel(excel_file, sheet, header = cutoff_index)
@@ -52,10 +53,10 @@ def read_data(input_directory, extensions):
                 for _, row in enumerate(df.itertuples(), 1):
                     # save the time of reading from the start of the experiment in seconds
                     if row[1] == "Time [s]":
-                        parsed_data[-1]['times'].append(row[2])
+                        parsed_data[-1].times.append(row[2])
                     # save the tempreture at the time of reading
                     elif row[1] == "Temp. [°C]":
-                        parsed_data[-1]['temps'].append(row[2])
+                        parsed_data[-1].temps.append(row[2])
                     # save the OD of the well
                     elif row[1] == "B" or row[1] == "C" or row[1] == "D" or row[1] == "E" or row[1] == "F" or row[1] == "G":
                         # Cnvert the character index to numaric index to be used to insert under the desired key in ODs
@@ -69,11 +70,11 @@ def read_data(input_directory, extensions):
                             # i is the index of the relevant cell within the excel sheet j is the adjusted value to make it zero based index to be used when saving to ODs
                             j = i - left_offset
                             curr_cell = (row_index, j)
-                            if curr_cell not in parsed_data[-1]['ODs']:
-                                parsed_data[-1]['ODs'][curr_cell] = [row[i]]
-                            # There is a previous reading for this cell, therefore normalize it against the first read than save it
+                            if curr_cell not in parsed_data[-1].ODs:
+                                parsed_data[-1].ODs[curr_cell] = [row[i]]
+                            # There is a previous reading for this cell, therefore normalize it against the first read then save it
                             else:
-                                parsed_data[-1]['ODs'][curr_cell].append(row[i] - parsed_data[-1]['ODs'][curr_cell][0])
+                                parsed_data[-1].ODs[curr_cell].append(row[i] - parsed_data[-1].ODs[curr_cell][0])
 
     return parsed_data
 
@@ -81,17 +82,17 @@ def create_graphs(data, output_path):
     # Loop all plates
     for experiment_data in data:
         # Loop all ODs within each plate
-        for row_index, columb_index in experiment_data['ODs']:
+        for row_index, columb_index in experiment_data.ODs:
             # Set the first value to 0 since it was used to normalize against
-            experiment_data['ODs'][(row_index, columb_index)][0] = 0
+            experiment_data.ODs[(row_index, columb_index)][0] = 0
 
             # Create the graph and save it
             fig, ax = plt.subplots()
-            ax.plot(experiment_data['times'], experiment_data['ODs'][(row_index, columb_index)])
+            ax.plot(experiment_data.times, experiment_data.ODs[(row_index, columb_index)])
             ax.set_xlabel('Time[s]')
             ax.set_ylabel('OD600')
             ax.set_title("ODs")
-            plt.savefig(output_path + "/well " + chr(row_index + 66) + "," + str(columb_index + 3) + " from " + experiment_data["file_name"] + " " + experiment_data["plate_name"])
+            plt.savefig(output_path + "/well " + chr(row_index + 66) + "," + str(columb_index + 3) + " from " + experiment_data.file_name + " " + experiment_data.plate_name)
             plt.close()
 
 def get_files_from_directory(path , extension):
