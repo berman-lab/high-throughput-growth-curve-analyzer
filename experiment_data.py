0f7f64239a7faf3a684b2d946860ca8cee6cce48
Carmi-Y <yeffet.carmi@gmail.com>
2021-11-08 18:04:53 +0200

    Added fields to hold curveball data

--- experiment_data.py
+++ experiment_data.py
@@ -2,8 +2,15 @@ from dataclasses import dataclass, field
 
 @dataclass()
 class ExperimentData:
-    ODs: field(default_factory=dict)
-    times: field(default_factory=list)
-    temps: field(default_factory=list)
-    plate_name: str
-    file_name: str
+    ODs : field(default_factory=dict)
+    times : field(default_factory=list)
+    temps : field(default_factory=list)
+    plate_name : str
+    file_name : str
+    begin_exponent_time : field(default_factory=dict)
+    # maximum population growth rate - denoted by a sometimes
+    max_population_gr : field(default_factory=dict)
+    # maximum of the per capita growth rate - denoted by μ sometimes
+    max_per_capita_gr : field(default_factory=dict)
+    # maximal poplution density -  denoted by k sometimes
+    max_population_density : field(default_factory=dict)
