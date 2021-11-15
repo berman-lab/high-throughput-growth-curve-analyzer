35314095f30e0dd176ca62181edb571a1998a4c1
Carmi-Y <yeffet.carmi@gmail.com>
2021-11-15 17:57:57 +0200

    Error loging and bug fixes

--- main.py
+++ main.py
@@ -6,27 +6,30 @@ import matplotlib.pyplot as plt
 from experiment_data import ExperimentData 
 
 def main():
-
+    # Base config and program parametrs
     base_path = "C:/Data/bio-graphs"
     # Input directory
     input_directory = base_path + "/In"
     # The directory into which all the graphs will be saved
     output_directory = base_path + "/Out"
     
+    err_log = []
+
     # Tuple with the all the extensions all the data files
     extensions = (".xlsx")
     
     # Get the data from the files
-    parsed_data = read_data(input_directory, extensions)
+    parsed_data = read_data(input_directory, extensions, err_log)
 
-    # Analysis data using curveball
-    get_growth_parameters(parsed_data)
+    # Analysis of the data using curveball
+    get_growth_parameters(parsed_data, err_log)
 
     # Graph the data and save the figures to the output_directory
-    create_graphs(parsed_data, output_directory, "New Title", verbos=False)
+    create_graphs(parsed_data, output_directory, "New Title", err_log, False)
 
+    save_err_log(output_directory, "Error log", err_log)
 
-def read_data(input_directory, extensions):
+def read_data(input_directory, extensions, err_log):
     '''Read all the data from the files with the given extension in the input directory given
     
      Parameters
@@ -44,9 +47,10 @@ def read_data(input_directory, extensions):
     --------   
     >>> read_data("Root/Folder/where_we_want_to_read_data_from", ("xslx", "csv"))
     '''
+
     # The index the data we want to analyze starts at
     #cutoff_index = 78
-    cutoff_index = 42
+    cutoff_index = 40
 
     # The container of the data after parsing but pre proccecing
     parsed_data = []
@@ -69,7 +73,7 @@ def read_data(input_directory, extensions):
                 # Load the current sheet of the excel file
                 df = pd.read_excel(excel_file, sheet, header = cutoff_index)
 
-                # run tourgh all the rows and columbs and save the data into object for graphing later
+                # run tourgh all the rows and columns and save the data into object for graphing later
                 # We use 96 well plates but only use the inner wells. That is, we treat the 96 well as a 60 well (6 X 10)
                 for _, row in enumerate(df.itertuples(), 1):
                     # save the time of reading from the start of the experiment in seconds
@@ -79,14 +83,15 @@ def read_data(input_directory, extensions):
                     elif row[1] == "Temp. [°C]":
                         parsed_data[-1].temps.append(row[2])
                     # save the OD of the well
+                    
                     elif row[1] in ["B", "C", "D" ,"E", "F", "G"]:
-                        # Cnvert the character index to numaric index to be used to insert under the desired key in ODs
+                        # Convert the character index to numaric index to be used to insert under the desired key in ODs
                         # 66 is the ASCII value of B and afterwards all the values are sequencial
                         row_index = ord(row[1]) - 66
 
-                        # This offset comes from the fact that in this expiremnt we don't right-most columb and the index given by itertuples
+                        # This offset comes from the fact that in this expiremnt we don't right-most column and the index given by itertuples
                         left_offset = 3
-                        # Collect all values from the columbs to ODs
+                        # Collect all values from the columns to ODs
                         for i in range(left_offset, 13):
                             # i is the index of the relevant cell within the excel sheet j is the adjusted value to make it zero based index to be used when saving to ODs
                             j = i - left_offset
@@ -102,7 +107,7 @@ def read_data(input_directory, extensions):
 
     return parsed_data
 
-def get_growth_parameters(data):
+def get_growth_parameters(data, err_log):
     '''
     Train a model and fill fields in the ExperimentData list given
     Parameters
@@ -119,34 +124,38 @@ def get_growth_parameters(data):
     '''
     tidy_df_list = create_tidy_dataframe_list(data)
 
+
+    # Use to retrive the needed data frame previosly created
     plate_num = 0
 
     # Loop all plates
     for experiment_data in data:
         # Loop all ODs within each plate and train model
-        for row_index, columb_index in experiment_data.ODs:
+        for row_index, column_index in experiment_data.ODs:
+            try:
+                key = (row_index, column_index)
+                current_model = curveball.models.fit_model(tidy_df_list[plate_num][key], PLOT = False)
+                begin_exponent_time = curveball.models.find_lag(current_model[0])
+                # max_growth stuff
+                # a - maximum population growth rate
+                # μ - maximum of the per capita growth rate
+                t1, y1, a, t2, y2, mu = curveball.models.find_max_growth(current_model[0])
 
-            key = (row_index, columb_index)
+                # K - maximum population density
+                max_population_density = current_model[0].params['K'].value
 
-            current_model = curveball.models.fit_model(tidy_df_list[plate_num][key], PLOT = False)
-            begin_exponent_time = curveball.models.find_lag(current_model[0])
-            # max_growth stuff
-            # a - maximum population growth rate
-            # μ - maximum of the per capita growth rate
-            t1, y1, a, t2, y2, mu = curveball.models.find_max_growth(current_model[0])
-
-            # K - maximum population density
-            max_population_density = current_model[0].params['K'].value
-
-            # Save model estimations to fields in the object
-            data[plate_num].begin_exponent_time[key] = begin_exponent_time
-            data[plate_num].max_population_gr[key] = a
-            data[plate_num].max_per_capita_gr[key] = mu
-            data[plate_num].max_population_density[key]= max_population_density
+                # Save model estimations to fields in the object
+                experiment_data.begin_exponent_time[key] = begin_exponent_time
+                experiment_data.max_population_gr[key] = a
+                experiment_data.max_per_capita_gr[key] = mu
+                experiment_data.max_population_density[key]= max_population_density
+            except Exception as e:
+                add_line_to_error_log(err_log, "Fitting of cell " + convert_wellkey_to_text(key) + " at plate: " + experiment_data.plate_name + 
+                 " failed with the following exception mesaage: " + str(e))
     
         plate_num += 1   
     
-def create_graphs(data, output_path, title, verbos=False):
+def create_graphs(data, output_path, title, err_log, verbos=False):
     '''Create graphs from the data collected in previous steps
     Parameters
     ----------
@@ -169,8 +178,8 @@ def create_graphs(data, output_path, title, verbos=False):
     # Loop all plates
     for experiment_data in data:
         # Loop all ODs within each plate
-        for row_index, columb_index in experiment_data.ODs:
-            key = (row_index, columb_index)
+        for row_index, column_index in experiment_data.ODs:
+            key = (row_index, column_index)
             
             # Set the first value to 0 since it was used to normalize against
             experiment_data.ODs[key][0] = 0
@@ -182,44 +191,42 @@ def create_graphs(data, output_path, title, verbos=False):
             ax.set_xlabel('Time [hours]')
             ax.set_ylabel('OD600')
 
-            plt.axhline(y=experiment_data.max_population_density[key], color='black', linestyle='-')
-            plt.axvline(x=experiment_data.begin_exponent_time[key], ymin=0, ymax=1, color='black')
-
-
-            # scalar = 0.025
-            # max_OD = experiment_data.ODs[key][-1]
-            # exponent_begin_OD = np.interp(experiment_data.begin_exponent_time[key], experiment_data.times, experiment_data.ODs[key])
-            # plt.axvline(x=experiment_data.begin_exponent_time[key], ymin=exponent_begin_OD - (max_OD * scalar), ymax=exponent_begin_OD + (max_OD * scalar), color='black')
-
-            
-
-
-            # Create a list of values in the best fit line
-            #abline_values = [slope * i + intercept for i in x]
-
-            # Plot the best fit line over the actual values
-            # plt.plot(x, y, '--')
-            # plt.plot(x, abline_values, 'b')
-            # plt.title(slope)
-            
-
-
-            # ax.legend()
-
+            # Plot the main graph
             ax.plot(experiment_data.times, experiment_data.ODs[key])
+
+            # Check if there are values or if the fitting failed for the cell
+            if key in experiment_data.max_population_density and key in experiment_data.begin_exponent_time :
+                plt.axhline(y=experiment_data.max_population_density[key], color='black', linestyle='-')
+                plt.axvline(x=experiment_data.begin_exponent_time[key], ymin=0, ymax=1, color='black')
+
+                # scalar = 0.025
+                # max_OD = experiment_data.ODs[key][-1]
+                # exponent_begin_OD = np.interp(experiment_data.begin_exponent_time[key], experiment_data.times, experiment_data.ODs[key])
+                # plt.axvline(x=experiment_data.begin_exponent_time[key], ymin=exponent_begin_OD - (max_OD * scalar), ymax=exponent_begin_OD + (max_OD * scalar), color='black')
+
+                # Create a list of values in the best fit line
+                #abline_values = [slope * i + intercept for i in x]
+
+                # Plot the best fit line over the actual values
+                # plt.plot(x, y, '--')
+                # plt.plot(x, abline_values, 'b')
+                # plt.title(slope)
+            
+            
+            # ax.legend()
 
             # Adds more data to help with graph analysis
             if verbos:
                 # Add vertical Lines
                 for xc in range (1, int(experiment_data.times[-1])):
                     plt.axvline(x=xc)
+                y = 0.05
+                while y <= max(experiment_data.ODs[(row_index, column_index)]):
+                    plt.axhline(y=y, color='r', linestyle='-')
+                    y += 0.05
 
-                # y = 0.05
-                # while y <= max(experiment_data.ODs[(row_index, columb_index)]):
-                #     plt.axhline(y=y, color='r', linestyle='-')
-                #     y += 0.05
-
-            plt.savefig(output_path + "/well " + chr(row_index + 66) + "," + str(columb_index + 3) + " from " + experiment_data.file_name + " " + experiment_data.plate_name)
+            # Save the figure
+            plt.savefig(output_path + "/well " + chr(row_index + 66) + "," + str(column_index + 3) + " from " + experiment_data.file_name + " " + experiment_data.plate_name)
             plt.close()
 
 def get_files_from_directory(path , extension):
@@ -242,19 +249,45 @@ def create_tidy_dataframe_list(data):
     --------   
     >>> tidy_df_list = create_tidy_dataframe_list(parsed_data)
     '''
-    # List of dictionaries of dataframes with a key of (row_index, columb_index) for each well in each plate
+    # List of dictionaries of dataframes with a key of (row_index, column_index) for each well in each plate
     result = []
 
     # Loop through all plates and collect the data in a way that is analyzeable by curveball down the line
     for experiment_data in data:
         # Create an empty dictionary to hold 
         result.append({})
-        for row_index, columb_index in experiment_data.ODs:
+        for row_index, column_index in experiment_data.ODs:
             # Create the dictionary that will be converted to the dataframe
-            d = {'Time': experiment_data.times, 'OD': experiment_data.ODs[(row_index, columb_index)], 'Temp[°C]': experiment_data.temps}
-            result[-1][(row_index, columb_index)] = pd.DataFrame(data=d)
+            d = {'Time': experiment_data.times, 'OD': experiment_data.ODs[(row_index, column_index)], 'Temp[°C]': experiment_data.temps}
+            result[-1][(row_index, column_index)] = pd.DataFrame(data=d)
 
     return result
+
+def convert_wellkey_to_text(key):
+    '''Converts a tuple of type (int,int) back to the appropriate human readable index 
+
+    Parameters
+    ----------
+    key : (int,int)
+        The key index of the well location
+
+    Returns
+    -------
+    str
+
+    Examples
+    --------   
+    >>> convert_wellkey_to_text(key)
+    '''
+    return chr(key[0] + 66) + "," + str(key[1] + 3)
+
+def add_line_to_error_log(log, new_line):
+    log.append(new_line)
+
+def save_err_log(path, name, err_log):
+    with open(path + "/" + name + ".txt", 'w') as file:
+        file.writelines("% s\n" % line for line in err_log)
+
 
 if __name__ == "__main__":
     main()
