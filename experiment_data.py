a267e237c6e2b0a132afd235c2f5d68e735ef47e
Carmi Yeffet <yeffet.carmi@gmail.com>
2022-01-16 12:39:29 +0200

    Removed unused prop +remaned temp2

--- experiment_data.py
+++ experiment_data.py
@@ -25,5 +25,5 @@ class ExperimentData:
     # maximal poplution density
     max_population_density : field(default_factory=dict)
 
-    temp1: field(default_factory=dict)
+    # The ODs if exponential growth had kept going
     exponent_ODs: field(default_factory=dict)
