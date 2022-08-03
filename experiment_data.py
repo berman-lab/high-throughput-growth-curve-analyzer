24bd5ba4000841fbb59cb01f8a93db3215df964d
Carmi Yeffet <yeffet.carmi@gmail.com>
2022-08-03 19:01:49 +0300

    No longer needed

--- experiment_data.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass, field
-
-@dataclass(kw_only=True, slots=True)
-class ExperimentData:
-    times : list[float] = field(default_factory=list)
-    temps : list[float] = field(default_factory=list)
-    plate_name : str
-    file_name : str
-    wells: dict = field(default_factory=dict) 
