1b00deb7d0cfdb9a94df766f09e49a19c0481276
Carmi <yeffet.carmi@gmail.com>
2022-08-04 19:10:23 +0300

    Sekeleton for growth parameters df

--- gc_core.py
+++ gc_core.py
@@ -1,8 +1,41 @@
-def get_well_growth_parameters(df):
+def get_experiment_growth_parameters(df):
     '''
-     Desrciption
+    Desrciption
     -----------
-    Get the growth pramaters from the curveball model trained on the data
+    Get a dataframe with all the growth parameters for each well in the experiment.
+    
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        A Dataframe containing all OD measurement for the well with the time of measurement . columns:
+        - ``filename`` (:py:class:`str`): the name of the file that is being analysed. [required]
+        - ``plate`` (:py:class:`str`): the name of the plate being analysed. : [required]
+        - ``well`` (:py:class:`str`): the well name, a letter for the row and a number of the column. [required]
+        - ``time`` (:py:class:`float`, in hours): [required]
+        - ``OD`` (:py:class:`float`, in AU): [required]
+        - ``temperature`` (:py:class:`float` in celcius): [optional]
+    Returns
+    -------
+    pandas.DataFrame
+        This dataframe will hold all the data gathered from the trained curveball models that were each fittef to a single well data. Dataframe containing the columns:
+        - ``filename`` (:py:class:`str`): the name of the file that is being analysed.
+        - ``plate`` (:py:class:`str`): the name of the plate being analysed.
+        - ``well`` (:py:class:`str`): the well name, a letter for the row and a number of the column.
+        - ``is_valid`` (:py:class:`bool`): True if the well is has passed fitting, False if there was a problem and the fitting process wan't complete.
+        - ``lag_end_time`` (:py:class:`float`, in hours): The time at which the lag phase ended.
+        - ``lag_end_OD`` (:py:class:`float`, in AU): The OD at which the lag phase ended.
+        - ``max_population_gr_time`` (:py:class:`float`, in hours): The time at which the maximum population growth rate (abbreviated as gr) was observed.
+        - ``max_population_gr_OD`` (:py:class:`float`, in AU): The OD at which the maximum population growth rate (abbreviated as gr) was observed.
+        - ``max_population_gr_slope`` (:py:class:`float`): The slope of the maximum population growth rate (abbreviated as gr).
+        - ``exponet_end_time`` (:py:class:`float`, in hours): The time at which the exponential phase ended, defined
+    '''
+    return 1
+
+def _get_well_growth_parameters(df):
+    '''
+    Desrciption
+    -----------
+    Get the growth pramaters from the curveball model trained on the data for a single well
     
     Parameters
     ----------
@@ -12,14 +45,19 @@ def get_well_growth_parameters(df):
         - ``OD`` (:py:class:`float`, in AU)
     Returns
     -------
-    pandas.DataFrame
-        This dataframe will hold all the data gathered from the trained curveball model paramaters. Dataframe containing the columns:
-        - ``filename`` (:py:class:`str`): the name of the file that is being analysed.
+    pandas.Series
+        This series will hold all the data gathered from the trained curveball model paramaters. datapoints:
+        - ``filename`` (:py:class:`str`): the name of the file the well belongs to.
         - ``plate`` (:py:class:`str`): the name of the plate being analysed.
         - ``well`` (:py:class:`str`): the well name, a letter for the row and a number of the column.
         - ``is_valid`` (:py:class:`bool`): True if the well is has passed fitting, False if there was a problem and the fitting process wan't complete.
         - ``lag_end_time`` (:py:class:`float`, in hours): The time at which the lag phase ended.
         - ``lag_end_OD`` (:py:class:`float`, in AU): The OD at which the lag phase ended.
-        TODO: Add the other parameters    
+        - ``max_population_gr_time`` (:py:class:`float`, in hours): The time at which the maximum population growth rate (abbreviated as gr) was observed.
+        - ``max_population_gr_OD`` (:py:class:`float`, in AU): The OD at which the maximum population growth rate (abbreviated as gr) was observed.
+        - ``max_population_gr_slope`` (:py:class:`float`): The slope of the maximum population growth rate (abbreviated as gr).
+        - ``exponet_end_time`` (:py:class:`float`, in hours): The time at which the exponential phase ended, defined as the time at which 95% of the carrying capcity, K, was first observed.
+        - ``exponet_end_OD`` (:py:class:`float`, in AU): The OD at which the exponential phase ended (95% of the carrying capcity, K, first observed).
+        - ``max_population_density`` (:py:class:`float`, in AU): The OD value determined to be the carrying capacity, K.
     '''
     return 1
