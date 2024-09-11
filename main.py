64a80864362ecb4529bbbbfb69d108ec0b8d1a95
Carmi Yeffet <yeffet.carmi@gmail.com>
2024-09-11 17:58:34 +0300

    Integrated changes

--- main.py
+++ main.py
@@ -127,14 +127,19 @@ def main():
         file_raw_data_df_mapping, file_summary_df_mapping, variation_matrix = gc_io.import_previous_run_data(output_path)
         print(f"Import succesful, imported {file_raw_data_df_mapping.keys()} raw data and summary data and the multiple files comprison table")
 
+
+    # Create a dir for the files from the last step
+    all_replicates_unified_data_path = gc_io.create_directory(output_path, 'all_replicates_unified_data')
+
     # For meeting with Judy and also bring some examples
     # gc_io.plot_dist(variation_matrix['relative_CC_score'])
     # gc_io.plot_dist(variation_matrix['max_CC_score_shift_in_hours'])
 
     # The data is in the needed objects, we can use it for the 
-    multiple_reps_and_files_summary_result = gc_core.multiple_reps_and_files_summary(condition_file_map ,file_condition_map, plate_repeats, file_raw_data_df_mapping, file_summary_df_mapping, variation_matrix)
+    unified_raw_data, unified_summary_data, all_invalid_wells_raw_data, all_invalid_wells_summary_data = gc_core.multiple_reps_and_files_summary(file_condition_map, plate_repeats, file_raw_data_df_mapping, file_summary_df_mapping, variation_matrix)
 
-    multiple_reps_and_files_summary_result.to_csv(os.path.join(output_path, 'multiple_reps_and_files_summary_result.csv'), index=False, encoding='utf-8')
+    gc_io.save_dataframe_to_csv(unified_raw_data, all_replicates_unified_data_path, 'raw_data_unified')
+    gc_io.save_dataframe_to_csv(unified_summary_data, all_replicates_unified_data_path, 'summary_data_unified')
 
     # create_reps_avarage_graphs(raw_data, averaged_rep, output_directory)
 
